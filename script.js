f9b3e40b6aa8b0437010859dc5ad4aebea0a1898
Hadi Deriszadeh <hadi.deris.h82@gmail.com>
2022-03-26 01:48:29 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function data(val) {
+  calc.display.value += val;
+}
+
+function ac() {
+  calc.display.value = "";
+}
+
+function c() {
+  calc.display.value = calc.display.value.slice(0, -1);
+}
+
+function equal() {
+  calc.display.value = eval(calc.display.value);
+}
+
